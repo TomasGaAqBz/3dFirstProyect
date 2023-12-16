06734cf9e117c87c9546cd8aa1e011920acd52a2
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2023-12-15 21:51:53 -0300

    Terminado las rutas y importacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Route, BrowserRouter as Router,Routes, } from 'react-router-dom'
 import './App.css'
+import {Home,Contact,About,Project} from './pages/index'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">
-      Hello world!
-    </h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/proyects' element={<Project/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+        
+      </Router>
+    </main>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './home/Home'
+import Contact from './contact/Contact'
+import About from './about/About'
+import Project from './projects/Projects'
+
+
+export {Home,Contact,About,Project}
