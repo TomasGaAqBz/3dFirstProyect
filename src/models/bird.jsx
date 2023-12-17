f72818ed0343ac37f186f0b5664c7ea638201efa
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2023-12-17 04:17:24 -0300

    El pajaro de mierda no andaba porque no le pase la ref al mesh LPM

--- src/models/bird.jsx
+++ src/models/bird.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useRef } from 'react'
-import { useAnimations, useGLTF } from '@react-three/drei'
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
 import birdScene from '../assets/3d/bird.glb'
 
 const bird = () => {
+  const birdRef= useRef()
 
-    const {scene,animations} = useGLTF(birdScene)
-    const birdRef= useRef()
-    const { actions } = useAnimations(animations,birdRef)
-    useEffect(() => {
-      actions["Take 001"].play();
-    }, [])
+  const { scene, animations } = useGLTF(birdScene);
+
+  const { actions } = useAnimations(animations, birdRef);
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
     
   return (
-    <mesh position={[-5,2,1]} scale={[0.003,0.003,0.003]}>
+    <mesh ref={birdRef} position={[-5,2,1]} scale={[0.003,0.003,0.003]}>
         <primitive object={scene}/>
     </mesh>
   )
